8fc651df91a87b1300ccc55674ab6cf68ee93df0
vhaohao23 <viethaodt123@gmail.com>
2025-10-08 22:06:38 +0700

    hybrid algorithm demo

--- UPGRADE-GDPSO.cpp
+++ UPGRADE-GDPSO.cpp
@@ -6,7 +6,7 @@ using namespace std;
 random_device rd;   
 mt19937 gen(rd());
 
-const int N=100;
+int N=100;
 const double c1=1.4961,c2=1.4961;
 const double w=0.7298;
 
@@ -141,7 +141,33 @@ vector<double> merge(vector<double> v1,vector<double> v2){
     return res;
 }
 
-
+void mutation(vector<int> &l,double u){
+    uniform_real_distribution<double> dis(0,1);
+    
+    int S = *max_element(l.begin(), l.end());
+    vector<int> ltmp;
+    for (int i=1;i<=n;i++){
+        double x=dis(gen);
+        if (x<u){
+            ltmp=l;
+            ++S;
+            double y=dis(gen);
+            if (y<0.5){                
+                l[i]=S;
+            }
+            else{
+                l[i]=S;
+                for (int neigbor:E[i])
+                    l[neigbor]=S;
+            }
+            
+            if (modularity(l)<=modularity(ltmp)){
+                --S;
+                l=ltmp;
+            }
+        }
+    }
+}
 
 void keyOperator(int t){
 
@@ -188,6 +214,98 @@ void keyOperator(int t){
     }
 }
 
+void consolidation(vector<int> &l,int l1,int l2){
+    for (int i=1;i<=N;i++)
+        if (l[i]==l1)
+            l[i]=l2;
+}
+
+void SecondaryCommunityConsolidation(vector<int>&l){
+    map<int,bool> mp;
+    map<int,int> cntNode;
+    int numC=0;
+    for (int i=1;i<=n;i++){
+        if (mp.find(l[i])==mp.end()){
+            ++numC;
+            mp[l[i]]=1;
+        }
+        cntNode[l[i]]++;
+    }
+
+    
+    vector<pair<int,int>> decComminities;
+    for (auto [x,_]:mp)
+        decComminities.push_back({x,cntNode[x]});
+    
+        sort(decComminities.begin(),decComminities.end(),[](pair<int,int> a,pair<int,int> b){
+        return a.second>b.second;
+    });
+
+    int i=numC-1;
+    vector<int> xtmp;
+    while (i>0){
+        int j=0;
+        bool check=0;
+        while (i>j){
+            xtmp=l;
+            consolidation(l,decComminities[i].first,decComminities[j].first);
+
+            if (modularity(l)>modularity(xtmp)){
+                --i;
+                check=1;
+                break;
+            }
+            else l=xtmp;
+            ++j;
+        }
+
+        if (!check) --i;
+    }
+    
+}
+
+void EPD(){
+    if (P.size()<10) return;
+
+    vector<pair<double, int>> modularityValues;
+    for (int i = 1; i <= N; i++) {
+        modularityValues.push_back({Q[i], i});
+    }
+
+    sort(modularityValues.begin(), modularityValues.end());
+
+    vector<vector<int>> sortedP(N + 1);
+    vector<vector<int>> sortedPb(N + 1);
+    vector<double> sortedQ(N + 1);
+    vector<double> sortedQb(N + 1);
+
+    for (int i = 0; i < N; i++) {
+        sortedP[i + 1] = P[modularityValues[i].second];
+        sortedPb[i + 1] = Pb[modularityValues[i].second];
+        sortedQ[i + 1] = Q[modularityValues[i].second];
+        sortedQb[i + 1] = Qb[modularityValues[i].second];
+    }
+
+    P=sortedP,Pb=sortedPb,Q=sortedQ,Qb=sortedQb;
+
+    double N_nor=N-(N/2+1)+1;
+    uniform_real_distribution<double> dis(0.0,1.0);
+    for (int i=N/2+1;i<=N;i++){
+        double C=1.0-exp(-double(i)/N_nor);
+        double rand=dis(gen);
+        if (rand<=C){
+            P.erase(P.begin() + i);
+            Pb.erase(Pb.begin() + i);
+            Q.erase(Q.begin() + i);
+            Qb.erase(Qb.begin() + i);
+            
+            --N;
+            --i;
+        }
+    }
+    
+}
+
 void GDPSO(){
     initialization();
 
@@ -211,7 +329,6 @@ void GDPSO(){
             vector<int> testVb=subtraction(Pb[i],P[i]);
             rep(i,1,n,1)
                 if (testVb[i]==1) cout<<"siuuu"; 
-
             vector<double> V2=multiplicationRcAndV(subtraction(Pg,P[i]),r2*c2);
 
             vector<double> Vtotal(n+1,0.0);
@@ -221,6 +338,7 @@ void GDPSO(){
 
             V[i]=merge(wVi,Vtotal);
             keyOperator(i);
+            mutation(P[i],0.3);
         }
         
 
@@ -242,6 +360,26 @@ void GDPSO(){
             if (Qb[i]>Qg)
                 Qg=Qb[i],Pg=Pb[i];
         }
+        EPD();
+    }
+
+    SecondaryCommunityConsolidation(Pg);
+    Qg=modularity(Pg);
+    for (int i=1;i<=N;i++){
+        SecondaryCommunityConsolidation(Pb[i]);
+        Qb[i]=modularity(Pb[i]);
+
+        SecondaryCommunityConsolidation(P[i]);
+        Q[i]=modularity(P[i]);
+        if (Q[i]>Qb[i]){
+            Qb[i]=Q[i];
+            Pb[i]=P[i];
+        }
+
+        if (Qb[i]>Qg){
+            Qg=Qb[i];
+            Pg=Pb[i];
+        }
     }
 
     cout<<Qg<<"\n";
@@ -250,6 +388,8 @@ void GDPSO(){
 }
 
 int main(){
+    clock_t tStart = clock();
+
     freopen("input.txt","r",stdin);
     // freopen("output.txt","w",stdout);
 
@@ -270,4 +410,6 @@ int main(){
     }
 
     GDPSO();
+
+    printf("\nTime taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 }
